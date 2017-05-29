7632f027b63994c324cec8d0bc19452927a524d9
Morel Cisse <Biloneofficiel@gmail.com>
2017-05-29 16:21:54 +0200

    Modifications

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ console.log('Hello');
 
 
 var situations = [{
-					situation : "situation numéro 1",
-					resA : "lala1",
-					resB : "lolo1",
-					response : "resA",
-					ifResA : "tu as donné la bonne reponse + message prevention",
-					ifResB : "tu as donné la mauvaise reponse + message prevention",
-					genre : "reseauSociaux.jpg"
-				},
-				{
-					situation : "situation numéro 2",
-					resA : "lala2",
-					resB : "lolo2",
-					response : "resB",
-					ifResA : "bravo, tu as donné la bonne reponse + message prevention",
-					ifResB : "dommage, tu as donné la mauvaise reponse + message prevention",
-					genre : "porn.jpg",
-				},];
+	situation : "Situation numéro 1",
+	resA : "Lala1",
+	resB : "Lolo1",
+	response : "resA",
+	ifResA : "Tu as donné la bonne reponse + Message prevention",
+	ifResB : "Tu as donné la mauvaise reponse + Message prevention",
+	genre : "reseauSociaux.jpg"
+},
+{
+	situation : "Situation numéro 2",
+	resA : "Lala2",
+	resB : "Lolo2",
+	response : "resB",
+	ifResA : "Bravo, Tu as donné la bonne reponse + Message prevention",
+	ifResB : "Dommage, Tu as donné la mauvaise reponse + Message prevention",
+	genre : "porn.jpg",
+},];
 
 
 
@@ -28,7 +28,7 @@ var situations = [{
 
 var Alea =Math.floor(Math.random()*situations.length);				
 function GenererPreventionAlea(){
-	$('#img').append("<img class='responsive-img' src='res/"+situations[Alea].genre+"'>")
+	$('#img').append("<img class='responsive-img' src='res/"+situations[Alea].genre+"'>");
 	$('#situation').html(situations[Alea].situation);
 	$('#resA').html(situations[Alea].resA);
 	$('#resB').html(situations[Alea].resB);
@@ -40,9 +40,9 @@ function reponsePreventionMessage() {
 		var test = $("#radioBtn input:checked").val();
 		console.log(test);
 		if(test === situations[Alea].response){
-			$("#reponsePrevention").html('<div class="card-panel light-green lighten-3">'+situations[Alea].ifResA+'</div>');
+			$("#reponsePrevention").html('<div class="card-panel light-green lighten-3 center">'+situations[Alea].ifResA+'</div>');
 		}else{
-			$('#reponsePrevention').html('<div class="card-panel red lighten-3">'+situations[Alea].ifResB+'</div>');
+			$('#reponsePrevention').html('<div class="card-panel red lighten-3 center">'+situations[Alea].ifResB+'</div>');
 		}
 	}else{
 		console.log("pas de reponse selectionnée");
