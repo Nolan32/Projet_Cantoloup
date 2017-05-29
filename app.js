1599ac719b236648729ea2dca41d096f82c26b16
Chauvin <chauvin-e@orange.fr>
2017-05-29 15:24:02 +0200

    corection

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 console.log('Hello');
 
-<<<<<<< HEAD
-var situations = [];
 
 var image = {
 	reseauSociaux:"res/reseauSociaux",
@@ -15,54 +13,29 @@ var image = {
 	hack:"res/hack",
 };
 
-var num1 = {
-	situation : "situation numéro 1",
-	resA : "bonne reponse",
-	resB : "mauvaise reponse",
-	response : "resA",
-	ifResA : "bravo, tu as donné la bonne reponse + message prevention",
-	ifResB : "dommage, tu as donné la mauvaise reponse + message prevention",
-=======
-var situations = [{
-					situation : "situation numéro 1",
-					resA : "lala1",
-					resB : "lolo1",
-					response : "resA",
-					ifResA : "bravo, tu as donné la bonne reponse + message prevention",
-					ifResB : "dommage, tu as donné la mauvaise reponse + message prevention"},
-				{
-					situation : "situation numéro 1",
-					resA : "lala2",
-					resB : "lolo2",
-					response : "resA",
-					ifResA : "bravo, tu as donné la bonne reponse + message prevention",
-					ifResB : "dommage, tu as donné la mauvaise reponse + message prevention"},];
 
-var Alea =Math.floor(Math.random()*situations.length);				
-function GenererPreventionAlea(){
-	// $('#img').append("<img class='responsive-img' src='/res/nom_de_l_image'>")
-	$('#situation').html(situations[Alea].situation);
-	$('#resA').html(situations[Alea].resA);
-	$('#resB').html(situations[Alea].resB);
->>>>>>> 8aa944e53386875c5279fc56dfd7724deff32f19
-};
+
+
+
 function preventionMessage() {
 	if ($("#radioBtn input").is(':checked')){
 		var test = $("#radioBtn input:checked").val();
 		console.log(test);
-		if(test === situations[Alea].response){
-			$("#reponsePrevention").html(situations[Alea].ifResA);
+		if(test === num1.response){
+			$("#reponsePrevention").html(num1.ifResA);
 		}else{
-			$('#reponsePrevention').html(situations[Alea].ifResB);
+			$('#reponsePrevention').html(num1.ifResB);
 		}
 	}else{
 		console.log("pas de reponse selectionnée");
 	}
 	
 }
-$(document).ready(function(){
-	GenererPreventionAlea();
-	$("#validate").click(function() {
-		preventionMessage();
-	});
-})
+
+
+
+
+
+$("#validate").click(function() {
+	preventionMessage();
+});
