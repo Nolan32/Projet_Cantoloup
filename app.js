46f901052648e9fd23b7207c3ef479d6aa2b7ce6
oceane <oceane.gatien.cef@hotmail.fr>
2017-05-31 11:30:54 +0200

    ajout situations

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ function GenererPreventionAlea(){
 	$('#situation').html(situations[Alea].situation);
 	$('#resA').html(situations[Alea].resA);
 	$('#resB').html(situations[Alea].resB);
-}	
+}
 function CreateButon(){
 	$('#CreateButon').append("<button class='btn waves-effect green darken-2' id='redirection'>www.google.fr</button>");
 	$('#CreateButon').delegate('#redirection','click',function(){
 		window.location.href ='http://www.google.fr';
 	});
 }
-function reponsePreventionMessage() {
+function reponsePreventionMessage(){
 
 	if ($("#radioBtn input").is(':checked')){
 		var test = $("#radioBtn input:checked").val();
@@ -21,31 +21,30 @@ function reponsePreventionMessage() {
 
 		if(test === situations[Alea].response){	
 
-			if (situations[Alea].response === "resA") {		
+			if (situations[Alea].response === "resA"){		
 				$("#reponsePrevention").html('<div class="card-panel light-green lighten-3 Panel">'+situations[Alea].ifResA+'</div>');
-				if (count===0) {
+				if (count===0){
 					setTimeout(CreateButon,1000);
 				}
 			}else{
 				$("#reponsePrevention").html('<div class="card-panel light-green lighten-3 Panel">'+situations[Alea].ifResB+'</div>');
-				if (count===0) {
+				if (count===0){
 					setTimeout(CreateButon,1000);
 				}
 			}
-		} else if (test !== situations[Alea].response){
+		}else if (test !== situations[Alea].response){
 
-			if (situations[Alea].response === "resB") {
+			if (situations[Alea].response === "resB"){
 				$('#reponsePrevention').html('<div class="card-panel red lighten-3 Panel">'+situations[Alea].ifResA+'</div>');
-				if (count===0) {
+				if (count===0){
 					setTimeout(CreateButon,1000);
-				}	
+				}
 			}else{
 				$('#reponsePrevention').html('<div class="card-panel red lighten-3 Panel">'+situations[Alea].ifResB+'</div>');
-				if (count===0) {
+				if (count===0){
 					setTimeout(CreateButon,1000);
 				}
 			}
-
 		}else{
 			console.log("pas de reponse selectionnée");
 			$('#reponsePrevention').html('<div class="card-panel red darken-1 Panel">Veuillez cochez la bonne réponse pour accéder à internet</div>');
--- prev.js
+++ prev.js
@@ -14,7 +14,7 @@ var situations = [{
 	response: "resB",
 	ifResA: "C’est con...ton “ami” était un serial killer... tu est mort dans d’horrible souffrance après avoir été séquestré pendant des semaines...",
 	ifResB: "Fais très attention aux rendez-vous que tu prends sur Internet! Tu peux te retrouver dans de très mauvaises situations (violence, vols, viols…), même si ce sont des 'amis' qui te proposent ce rendez-vous. Alors, parles-en à tes parents et ils te diront si tu peux y aller où s’ils viendront avec toi la première fois.",
-	genre : "reseauSociaux.jpg"
+	genre : "reseauSociaux.jpg",
 },
 {
 	situation: "Lors de tes discussions sur les réseaux sociaux, tu rentres en communication avec un pote que tu as rencontré lors d’une fête entre amis. Puis, tu te rends compte qu’il te demande beaucoup d’infos perso, sur toi et d’autres potes…",
@@ -69,22 +69,98 @@ var situations = [{
 	ifResA:"Dommage pour les parents d’Arthur, il vient de se faire attraper à avoir télécharger ce film et maintenant ils doivent payer une amende de 1500€.Les parent d’Arthur sont content de payer un film 1500€ au lieu de 20€ ! ",
 	ifResB:"Arthur évite d’une part des problèmes pour ces parents et sauve son ordinateur d’une mort certaine.", 
 	genre : "droit.jpg"
-}
+},
+{
+	situation: "Cela fait  plusieurs semaines que le petit-copain de Lily lui fait des avances insistantes via Internet. Il lui demande de faire des choses très osées.",
+	resA: "Elle refuse catégoriquement de faire quoi que ce soit. Elle en parle à un quelqu’un de confiance. Et sur les conseils de ses potes le quitte. ",
+	resB: "Amoureuse, elle cède à ses demandes à contre coeur.",
+	response: "resA",
+	ifResA: "La pornographie a un impact indéniable sur les violences faites aux femmes : la banalisation et la légitimation de rapports violents.",
+	ifResB: "NE CÈDE JAMAIS À UN CHANTAGE SEXUEL! En plus de te sentir humilié, ce genre d’acte peut être puni par la loi." ,
+	genre : "porn.jpg",
+},
+{
+	situation :  "Julien a appris que le sexe sert à faire des bébés et veux en savoir plus.",
+	resA : "Il demande plus d’informations aux adultes qui l’entourent, même si ça le gêne.",
+	resB : "Il lance une recherche internet et tombe sur des sites pornos. Autant apprendre seul ! ",
+	response : "resA",
+	ifResA : "Tu peux en parler avec tes parents, ou même les éducateurs. Il faut choisir le bon moment et le bon endroit pour pouvoir parler librement.",
+	ifResB : "Le jour où tu voudras apprendre à conduire...  regarder en boucle toute la série des  « TAXI », 1, 2, 3 et 4  ne t’aidera pas vraiment pour passer ton permis de conduire! Eh bien la pornographie, c’est pareil, c’est de la fiction!",
+	genre : "porn.jpg",
+},
+{
+	situation :  "Julie a télécharger illégalement quelques musiques de son groupe préféré. Puis, son ordinateur se bloque et un message d’Hadopi apparaît: ‘ Vous avez téléchargé illégalement, vous devez payer une amende de 350€ si vous voulez récupérer votre ordinateur’",
+	resA : "Julie doit payer tout de suite! Elle veut garder son ordinateur qui est devenu inutilisable et ne voulais pas transgresser la loi.",
+	resB : "Julie se méfie quand même et explique son erreur à un adulte !",
+	response : "resB",
+	ifResA : "Dommage, ce message d’Hadopi était un hacking, une personne mal-attentionnée à pris le contrôle de ton ordinateur et de tes données pour te soutirer de l’argent.",
+	ifResB : "Bien joué, ce message d’Hadopi était un hacking et ils voulaient te soutirer de l’argent.",
+	genre : "arnaques.png",
+},
+{
+	situation: "Une mère de famille reçoit un sms de sa banque avec un lien vers le site Internet pour des mises à jour.", 
+	resA: "Le message vient de sa banque, elle peut suivre le lien et faire les modifs.",
+	resB: "Elle contacte sa banque pour plus d’informations.",
+	response: "resB",
+	ifResA: "Elle clique sur le lien. C’est con … le site était un faux, un hacker lui a vider son compte… mourant de faim la famille n’a pas eu d’autre choix que de bouffer le chien et le petit dernier...",
+	ifResB: "Soyez attentifs lorsque votre service de banque en ligne est ouverte. Vous devez savoir quand et comment le système vous demandera de vous identifier, que ce soit lors de la connexion ou au moment de valider une transaction. Si vous voyez des demandes d'authentification à des moments inhabituels, n'entrez pas vos données et contactez votre banque.",
+	genre : "arnaques.png",
+},
+{
+	situation: "Au collège, un incident assez violent a éclaté entre Nicolas et ton pote Damien. Apparemment, Damien en voudrait à Nicolas et il veut se venger de lui. Il veut lui pourrir la vie sur Internet.",
+	resA: "Tu rigoles en même temps que Damien rédige de fausses rumeurs sur les réseaux sociaux. Et toi et tes potes relayez tout.", 
+	resB: "Au début tu rigoles, puis tu calme le jeu , mais comme tu vois que Damien est sérieux tu préfère prévenir un adulte.",
+	response: "resB",
+	ifResA: "Les fausses rumeurs se sont incontrôlablement propagés. Sous la pression Nicolas a fait une tentative de suicide.", 
+	ifResB: "Tu as bien fait. Le cyber-harcèlement est puni par la loi, au même titre que le harcèlement classique. Les premiers responsables en cas de harcèlement en ligne sont les auteurs des propos en cause.Un intermédiaire ne sera responsable que si : il a eu connaissance des messages publiés, et s'il n'a pas agi rapidement pour faire retirer ces messages dès qu'il en a eu connaissance.(www.service-public.fr)",
+	genre : "cyberHarcelement.jpg",
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+},
+{
+	situation: "Un père de famille laisse sa tablette entre les mains de ses enfants sans surveillance particulière.",
+	resA: "Et alors, il n’y a pas de dangers... ce ne sont que des jeux…",
+	resB: "Ce n’est pas une bonne idée. Les enfants ne devraient pas être sans surveillance même sur Internet.",
+	response: "resB",
+	ifResA: "L’un des deux a l’idée d’aller chercher des jeux sur Google. Malheureusement, ils voient du contenu pronographique. Sous le choc, l’un des deux meurt d’arrêt cardiaque et l’autre se défenestre s'empalant sur la balustrade du voisin.",
+	ifResB: "Ne laissez pas les jeunes enfants seuls sur Internet: 90% des enfants ont été au moins une fois confrontés à des contenus inadaptés. Signalez les images et propos pédopornographiques sur le site http://internet-signalement.gouv.fr qui est géré par la police. Plusieurs millions d’images pédopornographiques circulent en permanence sur le Web.",
+	genre : "controlParental.jpeg",
+},
+{
+	situation: "Des parents laissent leurs enfants passer toute la journée sur l’ordi, au point qu’ils ne font que ça.",
+	resA: "Il n’y a pas de problème. C’est leur choix.",
+	resB: "Ils essaient de les motiver pour faire autre chose. Et établissent des règles pour le temps de connexion.",
+	response: "resB",
+	ifResA: "A force de temps passé sur Internet, un des enfant à eu le cerveau liquéfié. L’autre a fait une crise d’épilepsie et a commencé à voir des hélicopter en liège partout. Et l’autre s’est perdu dans les toilettes.",
+	ifResB: "Limitez le temps consacré aux écrans: 45% des enfants de 6 à 11 ans consacrent plus de la moitié de leur temps de loisirs aux écrans: tv, Internet, jeux vidéo… ce qui peut nuire à la concentration scolaire.",
+	genre : "controlParental.jpeg"
+},
+{
+	situation : "Mona reçoit un mail de sa cousine, lui expliquant qu’elle a gagné 1500€ en répondant à un simple questionnaire en ligne! Elle lui conseil de faire vite car le tirage au sort est tous les jours à 19h.",
+	resA : "Mona tente sa chance, forcement, c’est pas rien 1500€ ! Et si sa cousine a gagné, c’est que le site est sur!",
+	resB : "Mona téléphone à sa cousine, pour avoir toutes les informations avant de s’inscrire.",
+	response : "resB",
+	ifResA : "Grâce aux informations entrer dans le formulaire, le compte en banque de Mona a été vidé et son identité a été volée. Le voleur a aussi transmis le mail à tous ses contacts et tente d’arnaquer avec ses proches.",
+	ifResB : "La cousine de Mona n’a jamais rien gagné! Elle se rendent compte alors que ce n'était qu’une arnaque !",
+	genre : "hack.jpg"
+},
+{
+	situation: "Cela fait quelque temps que tu remarques qu’un de tes pote à changer de comportement. Il se renferme sur lui-même, a des opinions de plus en plus tranché, surfe tout le temps sur les réseaux sociaux et s’éloigne de toi.",
+	resA: "Tu t’inquiètes, tu essaies de lui parler et tu parles de tes inquiétudes à un adulte.",
+	resB: "Il a le droit de penser ce qu’il veut... c’est pas à toi de gérer ses problèmes.",
+	response: "resA",
+	ifResA: "Dans la lutte contre le terrorisme et la radicalisation, le Gouvernement a ouvert deux comptes « Stop Djihadisme » sur Twitter et sur Facebook. Les réseaux sociaux constituent un outil incontournable, en relayant les messages tu contribueras à affaiblir la propagande.",
+	ifResB: "Ton pote a fait un gros kebab à la sortie de l’école… il s’est fait exploser. Des dizaines de gamins sont morts et d’autres ont été bléssé. D'ailleurs, un doigt n’a toujours pas été identifié…",
+	genre : "radicalisation.jpg"
+},
+{
+	situation: "Parce que bon élève Charles est le bouc émissaire de sa classe. Et un soir où il était dans sa chambre, il se rend compte que quelqu’un a posté des photos dégradantes de lui sur Internet. Le lendemain, tu vois tout la classe se moquer de lui.",
+	resA: "Tu as mal pour lui et même si tu ne l’apprécie pas trop tu préfères prévenir un adulte.",
+	resB: "C’est pas ton problème. A vraie dire, tu trouves les photos plutôt kiffantes.",
+	response: "resA",
+	ifResA: "Les cyberviolences n’ont pas de limite temporelle : elles peuvent s’exercer à toute heure du jour ou de la nuit et laissent des traces numériques (une photo publiée peut demeurer très longtemps sur Internet). L’auteur lui-même, une fois les agressions publiées sur la toile, ne peut maîtriser la diffusion des contenus.",
+	ifResB: "La diffusion des photos est devenu incontrôlable. Charles a pété le plombs et a commencé à manger de la craie pour calmer ses angoisses.",
+	genre : "cyberHarcelement.jpg",
+},
 
 
 
