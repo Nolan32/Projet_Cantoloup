6fb6eab4d90b6e307490494dbf663e072c0b9a4c
Chauvin <chauvin-e@orange.fr>
2017-06-01 12:23:13 +0200

    Finition anglais + modif prev.js

--- prev.js
+++ prev.js
@@ -5,7 +5,7 @@ var situations = [{
 	response : "resB",
 	ifResA : "Dommage, cet article est faux! Au moins, elle aurait fait rigoler ses ami(e)s !",
 	ifResB : "Et oui, toutes les informations sur internet ne sont pas à prendre au pied de la lettre ! Fais toi ton propre avis !",
-	genre : "recherche.jpg"
+	genre : "../../res/recherche.jpg"
 },
 {
 	situation: "Un copain de facebook, vient dans le Gers en vacance et t’invite à venir passer du temps avec lui. Tu lui fais confiance ou pas ?",
@@ -14,8 +14,7 @@ var situations = [{
 	response: "resB",
 	ifResA: "C’est con...ton “ami” était un serial killer... tu est mort dans d’horribles souffrances après avoir été séquestré pendant des semaines...",
 	ifResB: "Fait très attention aux rendez-vous que tu prends sur Internet ! Tu peux te retrouver dans de très mauvaises situations (violences, vols, viols…), même si ce sont des 'ami(e)s' qui te proposent ce rendez-vous. Alors, parles-en à tes parents et ils te diront si tu peux y aller où s’ils viendront avec toi la première fois.",
-	genre : "reseauSociaux.jpg",
-},
+	genre : "../../res/reseauSociaux.jpg"
 {
 	situation: "Lors de tes discussions sur les réseaux sociaux, tu rentres en communication avec un pote que tu as rencontré lors d’une fête entre ami(e)s. Puis, tu te rends compte qu’il te demande beaucoup d’infos persos sur toi et d’autres potes…",
 	resA: "Ce type est chelou, tu réfléchis et commences à lui poser des questions, ce qui le mets sur la défensive. Sur ce, tu préfère arrêter la communication.",
@@ -23,7 +22,7 @@ var situations = [{
 	response: "resA",
 	ifResA: "Si une personne que je ne connais pas et/ou mal, dans la vraie vie me demande des informations sur moi ou sur quelqu'un d’autre, un service, de l’argent, d’allumer la webcam, d’appeler un numéro de téléphone inconnu ou d’envoyer des photos, j’arrête tout de suite la communication, je me déconnecte et j’en parle immédiatement à mes parents.",
 	ifResB: "Dommage, ce “pote” est connu des services de polices pour des délits de cambriolages. Vu que tu lui as dit que tu partais voir de la famille ce week-end et que personnes sera chez toi… il est venu te rendre visite...et t’a chouré ta playstation ainsi que tous les ordis portables de la maison.",
-	genre : "hack.jpg",
+	genre : "../../res/hack.jpg"
 },
 {
 	situation:"Josiane vient de voir sur youtube une vidéo d’une personne qui a inventé une application qui permet d'avoir une balance sur smartphone pour peser de gros volume (par exemple une personne ).",
@@ -32,7 +31,7 @@ var situations = [{
 	response:"resB",
 	ifResA:"Oh bah dit donc, le téléphone s’est brisé lorsqu’elle a mit une partie de son poids sur le téléphone,c’est vraiment dommage elle va devoir se racheter un nouveau téléphone ",
 	ifResB:"Toutes les vidéo sur youtube ou autres sites sur internet ne sont pas forcément vraies ! Il faut avoir un regard critique sur ce que l’on voit.",
-	genre:"recherche.jpg"
+	genre: "../../res/recherche.jpg"
 },
 {
 	situation: "Tu passe beaucoup de temps sur ton compte facebook. En effet, cela te permets de vider ton esprit, de partager tes expériences et c’est ton plaisir.",
@@ -41,7 +40,7 @@ var situations = [{
 	response: "resB",
 	ifResA: "La diffusion Internet est incontrôlable. Même en prenant le plus de précautions possible. Une personne mal intentionnée peut retourner tes arguments contre toi et utiliser tes photos/vidéos à ton détriment et au détriment des personnes que tu aimes.",
 	ifResB:  "C’est bien que tu réfléchisses avant de publier quelque chose. Sur Internet tout le monde peut voir ce que tu met en ligne (photo, infos, opinions). Ne publie pas de photos gênantes de tes amis ou toi-même car leur diffusion est incontrôlable. Ton blog ou ton compte Facebook ne sont pas TON JOURNAL INTIME !",
-	genre : "droit.jpg"
+	genre : "../../res/droit.jpg"
 },
 {
 	situation: "Tu souhaites t’inscrire sur les réseaux sociaux. Pour cela tu dois remplir des formulaires.",
@@ -50,7 +49,7 @@ var situations = [{
 	response: "resB",
 	ifResA: "Un hacker passant par là… a récupéré le mot de passe et l’adresse email que tu as utilisé pour ton blog. Or, comme tu as utilisé bêtement les mêmes partout, toutes tes infos persos ont étée volées et ton ordi est HS.",
 	ifResB: "Tu créais plusieurs adresses électroniques qui sont différentes en fonction de tes activités : personnelles, professionnelles, associatives, ou sociales. Et tu, ne communiques pas tes mots de passe ! Et les choisis un peu compliqués (Ps: N'utilise jamais ta date de naissance ou ton nom/surnom).",
-	genre : "droit.jpg"
+	genre : "../../res/droit.jpg"
 },
 {
 	situation: "Florian joue à un jeu sur son téléphone mobile et souhaite avoir gratuitement des gemmes qui normalement sont payantes en euro.",
@@ -59,7 +58,7 @@ var situations = [{
 	response:"resB",
 	ifResA: "Quel dommage le téléphone de Florian s’est éteint dès qu’il a lancer l’application sur son téléphone et lorsqu’il a allumé, des pinguoins se sont mit à danser sur son fond d’écran",
 	ifResB:"Il faut se doutait qu’une application de ce genre est créé par un hacker et que s’il offre son application, c’est qu’il y a un fond de malveillance par derrière",
-	genre : "arnaques.png"
+	genre : "../../res/arnaques.png"
 },
 {
 	situation: "Arthur souhaite regarder le dernier film qui vient de sortir au cinéma.",
@@ -68,7 +67,7 @@ var situations = [{
 	response : "resB",
 	ifResA:"Dommage pour les parents d’Arthur, il vient de se faire attraper pour avoir télécharger ce film et maintenant ils doivent payer une amende de 1500€.Les parent d’Arthur sont content de payer un film 1500€ au lieu de 20€ ! ",
 	ifResB:"Arthur évite d’une part des problèmes pour ces parents et sauve son ordinateur d’une mort certaine.", 
-	genre : "droit.jpg"
+	genre : "../../res/droit.jpg"
 },
 {
 	situation: "Cela fait plusieurs semaines que le petit-copain de Lily lui fait des avances insistantes via Internet. Il lui demande de faire des choses très osées.",
